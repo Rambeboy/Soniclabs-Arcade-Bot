0491681302806eb63dca3f07bc192512b6de8d9a
R4MB3B0Y <kingindoslayer@gmail.com>
2024-11-15 11:25:32 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const REFERRER_CODE = 'tql092'
+export const REFERRER_CODE = 'b53121'
 export const RPC = {
   CHAINID: 64165,
   RPCURL: 'https://rpc.testnet.soniclabs.com',
